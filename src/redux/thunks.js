d4978861f723629c915e454663e59ae2d920b4da
Yonatan Lavie <yonatan.lavie89@gmail.com>
2021-06-09 19:34:22 +0300

    change fetching data with buitin fetch to axios.

--- src/redux/thunks.js
+++ src/redux/thunks.js
@@ -1,44 +1,20 @@
-// this file holds thunks that will fetch data from server
-
+import axios from 'axios';
 import { loadDataSuccess, loadDataInProgress, loadDataFailure, } from './actions'
+
+
 
 export const loadData = () => async (dispatch) => {
   try {
     // start fetching data -> switch isloading variable to true
     dispatch(loadDataInProgress());
 
-    fetch('https://s3-us-west-2.amazonaws.com/toluna-frontend-developer-test/data.json')
-    // Retrieve its body as ReadableStream
-    .then(response => response.body)
-    .then(rs => {
-      const reader = rs.getReader();
-
-      return new ReadableStream({
-        async start(controller) {
-          while (true) {
-            const { done, value } = await reader.read();
-
-            // When no more data needs to be consumed, break the reading
-            if (done) {
-              break;
-            }
-
-            // Enqueue the next data chunk into our target stream
-            controller.enqueue(value);
-          }
-
-          // Close the stream
-          controller.close();
-          reader.releaseLock();
-        }
-      })
-    })
-    // Create a new response out of the stream
-    .then(rs => new Response(rs))
-    // Create an object URL for the response
-    .then(response => response.json())
-    .then(json => dispatch(loadDataSuccess(json)))
-    .catch(console.error);
+    axios({
+      method: 'get',
+      url: 'https://s3-us-west-2.amazonaws.com/toluna-frontend-developer-test/data.json',
+      responseType: 'stream'
+    }).then((response) => {
+      dispatch(loadDataSuccess(response.data))
+    });
   } catch (e) {
     // error occurred -> switch isloading variable to false
     dispatch(loadDataFailure());
